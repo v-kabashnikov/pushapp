6b618645db07b2a8138b1355e94c048301def481
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-07-13 16:06:33 +0300

    unsasfe registration uno

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -2,7 +2,7 @@ class RegistrationController < ApplicationController
   skip_before_filter  :verify_authenticity_token
   def register
     respond_to do |format|
-      format.json { render :json => {answer: 'мглою'}}
+      format.json { render :json => {answer: 'снежные'}}
     end
   end
 end
