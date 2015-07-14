c14abedb61c4174094105f8e9ddee3224def4023
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-07-14 20:38:07 +0300

    worker test 6

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -3,15 +3,23 @@ class HardWorker
   sidekiq_options :retry => false
 
   def perform(question, level, task_id)
-    puts question
-    p = Poem.where("content like ?", "%" + question + "%").last.title
-    puts "dsds"
+    binding.pry
+    titles = Poem.where("content LIKE :query", query: "%#{question.strip}%").select(:title)
+    p = titles.first.title
     uri = URI("http://pushkin-contest.ror.by/quiz")
     parameters = {
         answer:p ,
         token: Token.last.user_token,
         task_id:  task_id
     }
-    Net::HTTP.post_form(uri, parameters)
+    puts parameters.to_s.magenta
+    r = Net::HTTP.post_form(uri, parameters)
+    p r.to_s.red
   end
 end
+
+
+
+
+
+  
