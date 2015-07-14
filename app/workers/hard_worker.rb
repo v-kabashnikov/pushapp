e115e8ac5170219bb140cf00a3eea5f040788343
v-kabashnikov <vania.kabashnikov@gmail.com>
2015-07-14 20:22:07 +0300

    worker test 3

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -3,8 +3,9 @@ class HardWorker
   sidekiq_options :retry => false
 
   def perform(question, level, task_id)
-    puts "dsds"
+    puts question
     p = Poem.where("content like ?", "%" + question + "%").last.title
+    puts "dsds"
     uri = URI("http://pushkin-contest.ror.by/quiz")
     parameters = {
         answer:p ,
